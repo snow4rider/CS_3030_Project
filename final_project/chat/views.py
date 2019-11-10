f159fe1a12a659e5fdb4c5b8e1afd09bc8075daf
Matthew <matthew.t.mckenney@gmail.com>
2019-11-09 19:02:49 -0700

    chat app
    added boot strap, a base html for easy updating and changes also added an about us page.

--- final_project/chat/views.py
+++ final_project/chat/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
 
-# Create your views here.
+# dummy data
+posts = [
+    {
+        'author': 'matthew',
+        'title': 'the day',
+        'content': 'the content'
+    },
+{
+        'author': 'jane',
+        'title': 'the day',
+        'content': 'the content'
+    }
+]
+
+
+def home(request):
+    context = {
+        'post': posts
+    }
+    return render(request, 'chat/home.html', context)
+
+
+def about(request):
+    return render(request, 'chat/about.html', {'title': 'About'})
