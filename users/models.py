7039f6f5ef86ebf681202dbbcb68001ca385e9e6
Matthew <matthew.t.mckenney@gmail.com>
2019-11-25 10:21:11 -0700

    Add files via upload

--- users/models.py
+++ users/models.py
@@ -16,6 +16,7 @@ class Profile(models.Model):
 
         img = Image.open(self.image.path)
 
+        # limits the size of the profile photo to 300px by 300px
         if img.height > 300 or img.width > 300:
             output_size = (300, 300)
             img.thumbnail(output_size)
